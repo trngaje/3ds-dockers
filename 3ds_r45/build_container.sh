213719875dc08e85f6361ac2d3e8f813c499818f
trngaje <bilbobaggins@naver.com>
2022-07-30 17:22:17 -0700

    update a docker for snes9x_3ds

---
+++ 3ds_r45/build_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker build -t trngaje3ds_r45 .
+mkdir export
---
+++ 3ds_r45/run_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run --rm -it  --volume $PWD/export:/home/user/export trngaje3ds_r45:latest
